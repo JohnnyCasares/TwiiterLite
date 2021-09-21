52ee5a3589725aadb2ae32929f2a3382b3305841
JohnnyCasares <jcasa097@fiu.edu>
2021-09-21 18:38:26 -0400

    updates

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -2,6 +2,7 @@ package com.codepath.apps.restclienttemplate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,6 +14,7 @@ import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
 import org.json.JSONException;
+import org.parceler.Parcels;
 
 import okhttp3.Headers;
 
@@ -58,7 +60,12 @@ public class ComposeActivity extends AppCompatActivity {
                         Log.e(TAG, "onSuccess to publish tweet");
                         try {
                             Tweet tweet = Tweet.fromJson(json.jsonObject);
-                            Log.i(TAG,"Tweet Published says: " + tweet);
+                            Log.i(TAG,"Tweet Published says: " + tweet.body);
+                            Intent intent = new Intent();
+                            intent.putExtra("tweet" , Parcels.wrap(tweet));
+                            //set result code and bundle data for response
+                            setResult(RESULT_OK, intent);
+                            finish(); // closes the activity, pass data to parent
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
--- app/src/main/java/com/codepath/apps/restclienttemplate/TimeLineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimeLineActivity.java
@@ -1,6 +1,7 @@
 package com.codepath.apps.restclienttemplate;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,6 +19,7 @@ import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +28,7 @@ import okhttp3.Headers;
 
 public class TimeLineActivity extends AppCompatActivity {
 
+    private final int REQUEST_CODE = 20;
     public static final  String TAG = "TimeLineActivity";
     TwitterClient client;
     RecyclerView rvTweets;
@@ -124,11 +127,30 @@ public class TimeLineActivity extends AppCompatActivity {
             //Toast.makeText(this,"compose", Toast.LENGTH_SHORT).show();
             //Navigate to the compose activity
             Intent intent = new Intent(this, ComposeActivity.class);
-            startActivity(intent);
+            startActivityForResult(intent, REQUEST_CODE);
             return true;
         }
         return super.onOptionsItemSelected(item);
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode ==  RESULT_OK){
+            //GET DATA FRM THE INTENT (TWEET)
+            Tweet tweet = Parcels.unwrap(data.getParcelableExtra("tweet"));
+            //The code above represents the tweet that we get back from what user composes in Compose Activity
+
+            //Update the RV with the tweet
+            //Modify data source of tweets
+            tweets.add(0, tweet);
+            //Update the adapter
+            adapter.notifyItemInserted(0);
+            rvTweets.smoothScrollToPosition(0);
+
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     private void populateHomeTimeLine() {
